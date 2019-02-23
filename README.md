88da4e5849ea114e8eb892d4d68d70df05c69b97
Peter Bengtsson <mail@peterbe.com>
2019-02-22 20:45:31 -0500

    basic implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+# Autocomplete With Images (in React)
+
+Demonstrating best practice of how to load images in an autocomplete
+widget.
---
+++ src/index.js
@@ -0,0 +1,114 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Highlighter from "react-highlight-words";
+import "./styles.css";
+
+function App() {
+  const [results, setResults] = React.useState(null);
+  const [resultsCount, setResultsCount] = React.useState(0);
+  const [search, setSearch] = React.useState("");
+
+  // Using sessionStorage is just a trick to avoid having to download
+  // all these every time you reload the app.
+  const [allPictures, setAllPictures] = React.useState(
+    JSON.parse(sessionStorage.getItem("allPictures") || "null")
+  );
+  React.useEffect(() => {
+    if (!allPictures) {
+      (async url => {
+        const res = await fetch(url);
+        const pictures = await res.json();
+        setAllPictures(pictures);
+        sessionStorage.setItem("allPictures", JSON.stringify(pictures));
+      })("https://picsum.photos/list");
+    }
+  }, [allPictures]);
+
+  React.useEffect(() => {
+    if (!search.trim()) {
+      return setResults(null);
+    }
+
+    const searchFor = search.toLowerCase();
+    const found = allPictures.filter(p => {
+      return (
+        p.author.toLowerCase().includes(searchFor) ||
+        (searchFor.length > 2 && p.filename.includes(search))
+      );
+    });
+    setResultsCount(found.length);
+    setResults(found.slice(0, 10));
+  }, [search]);
+
+  return (
+    <div className="App">
+      <h1>
+        <input
+          type="text"
+          placeholder="Type your search here..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+      </h1>
+
+      <ShowAutocompleteResults
+        search={search}
+        results={results}
+        count={resultsCount}
+      />
+      <hr />
+      <p>
+        <small>
+          {allPictures === null
+            ? "Downloading list of pictures..."
+            : `Downloaded list of ${allPictures.length.toLocaleString()} pictures`}
+        </small>
+      </p>
+    </div>
+  );
+}
+
+function ShowAutocompleteResults({ results, count, search }) {
+  if (!results) return null;
+  return (
+    <div className="suggestions">
+      <p className="meta">
+        Found {count.toLocaleString()}
+        {count > results.length && `, only showing first ${results.length}`}
+      </p>
+      {results.map(p => {
+        return (
+          <div key={p.id}>
+            <p>
+              <img
+                src={`https://picsum.photos/1000/1000?image=${p.id}`}
+                alt={p.filename}
+              />
+              <b>
+                <Highlighter
+                  searchWords={[search]}
+                  textToHighlight={p.author}
+                />
+              </b>
+            </p>
+            <p>
+              <code>
+                <Highlighter
+                  searchWords={[search]}
+                  textToHighlight={p.filename}
+                />
+              </code>
+              <br />
+              <small>
+                {p.width}x{p.height} ({p.format})
+              </small>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
